0a95d00f7a5cbfd9cf7c218247823cdb08f3fbbb
iskore <nicksoggin@gmail.com>
2019-02-25 09:26:12 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # geonet-leaflet-wmgs
 Leaflet.WMGS plugin (Web Map GeoJSON Service)
+
+### install
+
+```
+npm i -S @geonet/leaflet-wmgs
+```
+
+### Usage
+
+```
+const wmgs = new L.WMGS( {
+	crs: L.CRS.EPSG4326,
+	url: 'http://localhost:3000/wmgs',
+	// requestPerCell: true,
+	// filterTimeRange: {},
+	defaultStyle: {
+		color: 'red'
+	},
+	query: {},
+	// onCreateCell: ( bounds, coords ) => {},
+	// onCellEnter: ( bounds, coords ) => {},
+	// onCellLeave: ( bounds, coords ) => {},
+	pointToLayer: ( feature, latlng ) => {
+		return L.circleMarker( latlng, {
+			radius: 8,
+			fillColor: '#ff7800',
+			color: '#000',
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.4
+		} );
+	},
+	onEachFeature: ( geojson, layer ) => {
+		// Render the feature
+		layer.feature.properties.type = 'AOI';
+		this.drawLayer.addLayer( layer );
+	}
+} );
+
+wmgs.addTo( map );
+
+map.on( L.Draw.Event.CREATED, e => wmgs.createFeature( e ) );
+map.on( L.Draw.Event.EDITED, e => wmgs.updateFeatures( e ) );
+map.on( L.Draw.Event.DELETED, e => wmgs.deleteFeatures( e ) );
+```
+
+### Options
+
+| option            | default                                            | required | type     | description                                                                                                           |
+|-------------------|----------------------------------------------------|----------|----------|-----------------------------------------------------------------------------------------------------------------------|
+| `url`             |                                                    | true     | String   | URL for WMGS API                                                                                                      |
+| `CRS`             | `L.CRS.EPSG4326`                                   | false    | String   | Coordinate Reference Systems (default: equirectangular projection)                                                    |
+| `requestPerCell`  | `false`                                            | false    | Boolean  | Request GeoJSON one cell at a time, or group all cells together                                                       |
+| `filterTimeRange` | `{ from: 0, to: 0 }`                               | false    | Object   | Filter objects based on time range                                                                                    |
+| `defaultStyle`    | `{ color: 'blue', opacity: 1, fillOpacity: 0.4 }`  | false    | Object   | Default polygon styling                                                                                               |
+| `query`           | `{}`                                               | false    | Object   | Define query parameters to filter all responses by                                                                    |
+| `select`          | `{ type: true, properties: true, geometry: true }` | false    | Object   | Define parameters to select from DB (`type`, `properties`, and `geometry` must be defined to properly render GeoJSON) |
+| `onCreateCell`    |                                                    | false    | Function | Called when a new cell is created                                                                                     |
+| `onCellEnter`     |                                                    | false    | Function | Called when an existing cell is entered                                                                               |
+| `onCellLeave`     |                                                    | false    | Function | Called when an existing cell is exited                                                                                |
+| `pointToLayer`    |                                                    | false    | Function | Function that will be used for creating layers for GeoJSON Points.                                                    |
+| `onEachFeature`   |                                                    | false    | Function | Function used to add GeoJSON to the map or allows for inspection of GeoJSON Features.                                 |
+
+### Methods
+
+| method             | params                       | returns                    | description                                                                                                    |
+|--------------------|------------------------------|----------------------------|----------------------------------------------------------------------------------------------------------------|
+| `fetch`            | Object (request options)     | `{Promise<*>}`             | Used to request the WMGS server                                                                                |
+| `customRequest`    | Object (request options)     | `{Promise<*>}`             | Allows user to make custom defined request to the WMGS server                                                  |
+| `getTypes`         |                              | Array                      | Get the types of Features defined in the WMGS server                                                           |
+| `getFeatures`      | `bounds` (`L.latLng` object) | Object (FeatureCollection) | Method used by module to request GeoJSON from WMGS. Method will pass all retrieved layers into `onEachFeature` |
+| `createFeature`    | `Layer`                      | Fetch response             | Add a WMGS Feature                                                                                             |
+| `updateFeatures`   | `Layer[]`                    | Fetch response             | Update WMGS Features                                                                                           |
+| `deleteFeatures`   | `Layer[]`                    | Fetch response             | Delete WMGS Features                                                                                           |
+| `timeFromObjectId` | `ObjectID`                   | `Date`                     | Converts an `ObjectID` into a `Date`                                                                           |
+| `objectIdToTime`   | `Date`                       | `ObjectID`                 | Converts a `Date` into an `ObjectID`                                                                           |
---
+++ index.js
@@ -0,0 +1,352 @@
+/** ****************************************************************************************************
+ * File: index.js
+ * Project: geonet-leaflet-wmgs
+ * @author Nick Soggin <iSkore@users.noreply.github.com> on 24-Feb-2019
+ *******************************************************************************************************/
+'use strict';
+
+import { Util, GeoJSON, geoJSON, latLng, latLngBounds, CRS } from 'leaflet';
+
+import VirtualGrid from 'leaflet-virtual-grid';
+
+import Response from 'http-response-class';
+import LightMap from '@parellin/lightmap';
+
+L.WMGS = VirtualGrid.extend( {
+	options: {
+		crs: CRS.EPSG3857,
+		url: '',
+		// maxFeatures: null,
+		requestPerCell: false,
+		filterTimeRange: Object.seal( {
+			from: 0,
+			to: 0
+		} ),
+		query: {},
+		select: { type: true, properties: true, geometry: true },
+		defaultStyle: {
+			color: 'blue',
+			opacity: 1,
+			fillOpacity: 1
+		}
+	},
+	
+	_cache: new LightMap(),
+	_activeRequests: 0,
+	currentTimeRange: Object.seal( {
+		from: 0,
+		to: 0
+	} ),
+	
+	currentVisibleTypes: new Set(),
+	
+	initialize( opts ) {
+		VirtualGrid.prototype.initialize.call( this, opts );
+		this.on( 'cellsupdated', this._cellsUpdated );
+	},
+	
+	_update() {
+		VirtualGrid.prototype._update.call( this );
+	},
+	
+	async fetch( opts ) {
+		try {
+			opts._body = opts.body;
+			
+			if( opts.headers[ 'Content-Type' ].startsWith( 'application/json' ) ) {
+				opts.body = JSON.stringify( opts.body );
+			}
+			
+			const res = await fetch( this.options.url, opts );
+			
+			if( !Response.isSuccess( res.status ) ) {
+				return Promise.reject( await res.json() );
+			}
+			
+			if( opts.headers[ 'Content-Type' ].startsWith( 'application/json' ) ) {
+				return await res.json();
+			}
+			
+			return res;
+		} catch( e ) {
+			return Promise.reject( e );
+		}
+	},
+	
+	async customRequest( opts ) {
+		return await this.fetch( {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: opts
+		} );
+	},
+	
+	async getTypes() {
+		return await this.customRequest( {
+			action: 'getTypes',
+			layer: 'default',
+			property: 'properties.type'
+		} );
+	},
+	
+	async getFeatures( bounds ) {
+		this._activeRequests++;
+		
+		if( this._activeRequests === 1 ) {
+			this.fire( 'loading', { bounds }, true );
+		}
+		
+		try {
+			const featureRequest = {
+				action: 'getItems',
+				layer: 'default',
+				zoom: this._map.getZoom(),
+				query: {
+					_id: { $nin: [ ...this._cache.keys() ] },
+					geometry: {
+						$geoIntersects: {
+							$geometry: {
+								type: 'Polygon',
+								coordinates: [ [
+									[ bounds._southWest.lng, bounds._southWest.lat ],
+									[ bounds._northEast.lng, bounds._southWest.lat ],
+									[ bounds._northEast.lng, bounds._northEast.lat ],
+									[ bounds._southWest.lng, bounds._northEast.lat ],
+									[ bounds._southWest.lng, bounds._southWest.lat ]
+								] ]
+							}
+						}
+					},
+					...this.options.query
+				},
+				select: this.options.select
+			};
+			
+			if( this.options.filterTimeRange.from ) {
+				featureRequest.query._id.$gte = this.objectIdToTime( this.options.filterTimeRange.from );
+			}
+			
+			if( this.options.filterTimeRange.to ) {
+				featureRequest.query._id.$lte = this.objectIdToTime( this.options.filterTimeRange.to );
+			}
+			
+			const featureCollection = await this.fetch( {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: featureRequest
+			} );
+			
+			if( featureCollection && featureCollection.features.length ) {
+				// Immediately make cache reference so we don't request the same feature n * cell number of times
+				for( let i = 0; i < featureCollection.features.length; i++ ) {
+					this._cache.set( featureCollection.features[ i ]._id, null );
+				}
+				
+				Util.requestAnimFrame(
+					() => {
+						this._addFeatures( featureCollection.features );
+						// this._postProcessFeatures( bounds );
+					}
+				);
+			}
+			
+			return featureCollection;
+		} catch( e ) {
+			return Promise.reject( e );
+		}
+	},
+	
+	_createNewLayer( geojson ) {
+		let layer;
+		
+		if( geojson.geometry.type === 'Point' ) {
+			if( this.options.pointToLayer ) {
+				layer = this.options.pointToLayer(
+					geojson,
+					latLng(
+						geojson.geometry.coordinates[ 1 ],
+						geojson.geometry.coordinates[ 0 ]
+					)
+				);
+			} else {
+				layer = geoJSON();
+			}
+		} else {
+			layer = GeoJSON.geometryToLayer( geojson );
+		}
+		
+		if( layer ) {
+			layer.defaultOptions = layer.options;
+			layer.feature        = geojson;
+			
+			// if it's a point, don't style it
+			if( geojson.geometry.type !== 'Point' ) {
+				if( layer.setStyle && layer.feature.properties ) {
+					if( layer.feature.properties.hasOwnProperty( 'style' ) ) {
+						layer.setStyle( layer.feature.properties.style || this.options.defaultStyle );
+					} else {
+						layer.setStyle( this.options.defaultStyle );
+					}
+				} else {
+					layer.setStyle( this.options.defaultStyle );
+				}
+			}
+		}
+		
+		return layer;
+	},
+	
+	_buildIndexes() {
+		let timeframe = [];
+		
+		for( const val of this._cache.values() ) {
+			timeframe.push( this.timeFromObjectId( val.feature._id ) );
+			this.currentVisibleTypes.set( val.feature.properties.type );
+		}
+		
+		timeframe = timeframe.sort();
+		
+		this.currentTimeRange.from = timeframe[ 0 ];
+		this.currentTimeRange.to   = timeframe[ timeframe.length - 1 ];
+	},
+	
+	_addFeatures( features ) {
+		for( let i = 0; i < features.length; i++ ) {
+			const
+				ref   = features[ i ],
+				layer = this._createNewLayer( ref );
+			
+			if( this.options.onEachFeature ) {
+				this.options.onEachFeature( layer.feature, layer );
+			}
+			
+			this._cache.set( ref._id, layer );
+		}
+		
+		this._buildIndexes();
+	},
+	
+	async createFeature( e ) {
+		if( e.layer ) {
+			e = e.layer;
+		}
+		
+		const geojson = e.toGeoJSON();
+		
+		geojson.properties = geojson.properties || {};
+		
+		e = this._createNewLayer( geojson );
+		
+		if( this.options.onEachFeature ) {
+			this.options.onEachFeature( e.feature, e );
+		}
+		
+		return await this.fetch( {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: {
+				action: 'createItem',
+				layer: 'default',
+				data: geojson
+			}
+		} );
+	},
+	
+	async updateFeatures( e ) {
+		const
+			layers  = e.layers.getLayers(),
+			updates = [];
+		
+		for( let i = 0; i < layers.length; i++ ) {
+			updates.push(
+				await this.fetch( {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: {
+						action: 'updateItem',
+						layer: 'default',
+						query: { _id: layers[ i ]._id },
+						data: layers[ i ].feature
+					}
+				} )
+			);
+		}
+		
+		return Promise.all( updates );
+	},
+	
+	async deleteFeatures( e ) {
+		const _id = e.layers.getLayers().map( layer => layer.feature._id );
+		
+		if( !_id.length ) {
+			return _id;
+		}
+		
+		return await this.fetch( {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: {
+				action: 'deleteItem',
+				layer: 'default',
+				data: { _id }
+			}
+		} );
+	},
+	
+	async _cellsUpdated() {
+		const
+			cells         = Object.keys( this._cells ),
+			requestBounds = latLngBounds();
+		
+		for( let i = 0; i < cells.length; i++ ) {
+			if( this.options.requestPerCell ) {
+				await this.getFeatures( this._cells[ cells[ i ] ].bounds );
+			} else {
+				requestBounds.extend( this._cells[ cells[ i ] ].bounds );
+			}
+		}
+		
+		if( !this.options.requestPerCell ) {
+			await this.getFeatures( requestBounds );
+		}
+	},
+	
+	createCell( bounds, coords ) {
+		if( this.options.onCreateCell ) {
+			this.options.onCreateCell( bounds, coords );
+		}
+	},
+	
+	cellEnter( bounds, coords ) {
+		if( this.options.onCellEnter ) {
+			this.options.onCellEnter( bounds, coords );
+		}
+	},
+	
+	cellLeave( bounds, coords ) {
+		if( this.options.onCellLeave ) {
+			this.options.onCellLeave( bounds, coords );
+		}
+		
+		for( const [ k, v ] of this._cache ) {
+			if( v.feature.geometry.type === 'Point' ) {
+				if( bounds.contains( v.getLatLng() ) ) {
+					this._map.removeLayer( v );
+					this._cache.delete( k );
+				}
+			} else {
+				if( bounds.contains( v.getBounds() ) ) {
+					this._map.removeLayer( v );
+					this._cache.delete( k );
+				}
+			}
+		}
+	},
+	
+	timeFromObjectId( _id ) {
+		return new Date( Number.parseInt( _id.substring( 0, 8 ), 16 ) * 1000 );
+	},
+	objectIdToTime( date ) {
+		return Math.floor( new Date( date ).getTime() / 1000 ).toString( 16 ) + '0'.repeat( 16 );
+	}
+} );
